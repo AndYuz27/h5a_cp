5c7bd79065e6c5b8018b95ebbba6629db8c968a7
AndYuz27 <yuzlov2003@gmail.com>
2022-11-17 15:46:57 +0300

    sample text

--- script.js
+++ script.js
@@ -5,41 +5,10 @@ console.log(main)
 
 
 let ctx = canvas.getContext('2d');
-ctx.fillStyle = "black";
-ctx.lineWidth = 2.0;
-ctx.beginPath(); 
-ctx.moveTo(30, 10); 
-ctx.lineTo(30, 460); 
-ctx.lineTo(500, 460); 
-ctx.stroke(); 
+
 
 function addGraph(){
 
-}
-
-ctx.fillStyle = "black";
-for(let i = 0; i < 6; i++) { 
-    ctx.fillText((5 - i) * 20 + "", 4, i * 80 + 60); 
-    ctx.beginPath(); 
-    ctx.moveTo(25, i * 80 + 60); 
-    ctx.lineTo(30, i * 80 + 60); 
-    ctx.stroke(); 
-}
- 
-let labels = []; 
-// let labels = ["graph1", "graph2", "graph3", "graph4"];
-let addLabel = labels.push("graph1", "graph2", "graph3", "graph4")
- 
-for(var i=0; i<4; i++) { 
-    ctx.fillText(labels[i], 50+ i*100, 475); 
-}
-let data = [ 10, 53, 39, 54]; 
-// let data = [ 10, 53, 39, 54]; 
- 
-ctx.fillStyle = "blue"; 
-for(var i=0; i<data.length; i++) { 
-    var dp = data[i]; 
-    ctx.fillRect(40 + i*100, 460-dp*5 , 50, dp*5); 
 }
 
 
@@ -47,16 +16,6 @@ for(var i=0; i<data.length; i++) {
 
 const fff = function(){ //IIFE
 
-  let arr = ["https://www.pngpix.com/wp-content/uploads/2016/08/PNGPIX-COM-Cocktail-Glass-PNG-Transparent-Image.png", "https://www.freepngimg.com/thumb/tea/35822-6-iced-tea-transparent.png", "https://free-png.ru/wp-content/uploads/2022/06/free-png.ru-731.png", "https://nalivay-ka.ru/wp-content/uploads/2017/03/i-1-8-1024x768.jpg"]
-
-function arrImg(eee){
-  eee.map(function(element, index, array){
-    console.log(element);
-
-    return element;
-}, 80);
-}
-arrImg(arr)
 
 
 
@@ -68,27 +27,67 @@ arrImg(arr)
 console.log(el.brandname)
 if(el.brandname) {
 
-let arr2 = [el.price1, el.price2, el.price3, el.price4]
-let arr3 = [el.date1, el.date2,el.date3, el.date4]
+let arr2 = [el.price1, el.price2, el.price3, el.price4] // массив с ценами
+let arr3 = [el.date1, el.date2,el.date3, el.date4] // массив с датой
 
 console.log(arr2)
 console.log(arr3)
 
 
   let card = `
-  <div id="card">
+  <div id="card" draggable = true data-price=${arr2} data-date=${arr3}>
           <h4>Brand name: ${el.brandname}</h4>
           
           </div>
 `;
 
-let graph = ""
 console.log(card)
       main.innerHTML += card;
-      let crdl = document.getElementById("card")
-crdl.draggable = true
+
+let crdl = document.getElementById("card")
+let graphEl = document.getElementById('graph')
+console.log(graphEl)
 crdl.ondragstart = () => console.log("start")
 crdl.ondragend = () => console.log("end")
+console.log( "Данные с карточек ",crdl.dataset.price)
+graphEl.ondrangenter = () => console.log("enter")
+graphEl.ondrangover = () => console.log("over")
+graphEl.ondrangleave = () => console.log("leave")
+
+
+ctx.fillStyle = "black";
+ctx.lineWidth = 2.0;
+ctx.beginPath(); 
+ctx.moveTo(30, 10); 
+ctx.lineTo(30, 460); 
+ctx.lineTo(500, 460); 
+ctx.stroke(); 
+
+ctx.fillStyle = "black";
+for(let i = 0; i < 6; i++) { 
+    ctx.fillText((5 - i) * 20 + "", 4, i * 80 + 60); 
+    ctx.beginPath(); 
+    ctx.moveTo(25, i * 80 + 60); 
+    ctx.lineTo(30, i * 80 + 60); 
+    ctx.stroke(); 
+}
+ 
+let labels = []; 
+let addLabel = labels.push("graph1", "graph2", "graph3", "graph4")
+ 
+for(var i=0; i<4; i++) { 
+    ctx.fillText(labels[i], 50+ i*100, 475); 
+}
+let data = [el.price1, el.price2, el.price3, el.price4]; 
+// let data = [ 10, 53, 39, 54]; 
+ 
+ctx.fillStyle = "blue"; 
+for(var i=0; i<data.length; i++) { 
+    var dp = data[i]; 
+    ctx.fillRect(40 + i*110, 460-dp*5 , 50, dp*5); 
+}
+
+
 
 }
 
